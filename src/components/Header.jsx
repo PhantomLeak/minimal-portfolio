39cd6a33d6ff23ff6e92e25b2ffaa569ba28f0b1
Phantomleak <dstocking0823@gmail.com>
2022-11-27 11:32:33 -0500

    add header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react'
+
+function Header() {
+    const [theme, setTheme] = useState(null);
+
+    useEffect(() => {
+		if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+			setTheme('dark');
+		} else {
+			setTheme('light');
+		}
+	}, []);
+
+	useEffect(() => {
+		if (theme === 'dark') {
+			document.documentElement.classList.add('dark');
+		} else {
+			document.documentElement.classList.remove('dark');
+		}
+	}, [theme]);
+
+    const handleThemeSwitch = () => {
+        setTheme(theme === 'dark' ? 'light' : 'dark');
+    };
+
+	return (
+		<div className='container max-w-screen-lg mx-auto'>
+            {/* button to change light / dark mode */}
+            <button
+                type="button"
+                onClick={handleThemeSwitch}
+                className="fixed p-2 z-10 right-5 top-4 bg-violet-300 dark:bg-orange-300 text-lg p-1 rounded-md"
+            >
+                {theme === 'dark' ? <i class="fa-solid fa-sun"/> : <i class="fa-solid fa-moon"/>}
+            </button>
+		</div>
+	);
+}
+
+export default Header;
