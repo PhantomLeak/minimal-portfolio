a4e8f0b4e4bc0e3d0242a8500453d16be0814ff2
Phantomleak <dstocking0823@gmail.com>
2022-11-27 11:57:59 -0500

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/minimal-portfolio/', // Base URL for the site
   plugins: [react()]
 })
