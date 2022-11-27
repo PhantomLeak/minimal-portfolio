be959d857fe93d3f627b651ef6c9c4937ec86fcf
Phantomleak <dstocking0823@gmail.com>
2022-11-27 12:19:31 -0500

    Small styling changes and testing rebuilding site

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -11,7 +11,7 @@ export default [
         title: "Bachelor's Degree in Computer Information Systems",
         duration: '5 years',
         details:
-            'I pursued the knowledge that a computer science degree had to offer. I learned about computer architecture, computer science theory, an array of programming, and aspects of artificial intelligence.',
+            'I pursued the knowledge that a computer information systems degree had to offer. I learned about computer architecture, computer science theory, an array of programming, and aspects of artificial intelligence.',
     },
     {
          year: '2021',
