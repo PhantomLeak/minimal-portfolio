c2f4b2218adac8aabb7ec88759cd5483c4606dd5
Phantomleak <dstocking0823@gmail.com>
2023-02-05 15:32:42 -0500

    Adding dist

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/minimal-portfolio/', // Base URL for the site
-  plugins: [react()]
-})
