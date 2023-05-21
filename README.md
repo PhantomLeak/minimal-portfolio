57b01c8f29c40f1974f679903d07f882a7ba0074
Phantomleak <dstocking0823@gmail.com>
2023-05-21 14:37:29 -0400

    Adding dist

--- README.md
+++
@@ -1,4 +0,0 @@
-# minimal-portfolio
-Personal site written in reactjs, HTML5, and tailwind using vite
-## Site link
-https://phantomleak.github.io/minimal-portfolio/
--- src/components/GitHubComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function GitHubComponent() {
-   return (
-    <div className="github-widget" data-user="PhantomLeak" />
-   )
-}
-
-export default GitHubComponent;
