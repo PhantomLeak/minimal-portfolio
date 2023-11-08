a961b317cce2f8315f30654e358e54323facba43
Dylan Stocking <dstocking0823@gmail.com>
2023-11-08 18:42:17 -0500

    Start bookshelf

---
+++ src/data/books.js
@@ -0,0 +1,7 @@
+export default [
+    {
+        year: '2023',
+        title: 'A Brief Answer To The Big Questions',
+        url: 'http://www.32books.com/wp-content/uploads/2018/10/Brief-answers-to-the-Big-Questions-600x879.jpg'
+    }
+]
